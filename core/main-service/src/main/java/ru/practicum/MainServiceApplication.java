f590ed3ee9a861bee38b0ed865008149c172e506
Denis585585 <Vegan585@yandex.ru>
2025-08-08 13:55:20 +0300

    Добавление Spring Cloud

--- core/main-service/src/main/java/ru/practicum/MainServiceApplication.java
+++ core/main-service/src/main/java/ru/practicum/MainServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"stat", "ru.practicum"})
+@ComponentScan(basePackages = {"stats", "ru.practicum"})
 public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
