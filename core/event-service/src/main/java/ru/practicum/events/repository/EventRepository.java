d0842a0b424d14ba28db2c838d85f9bf1ab1d217
Denis585585 <Vegan585@yandex.ru>
2025-08-13 04:25:37 +0300

    Fix EventRepository

--- core/event-service/src/main/java/ru/practicum/events/repository/EventRepository.java
+++ core/event-service/src/main/java/ru/practicum/events/repository/EventRepository.java
@@ -21,7 +21,7 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
     Optional<Event> findByIdAndInitiatorId(Integer eventId, Integer userId);
 
     @Query("SELECT e FROM Event e " +
-            "WHERE (:users IS NULL OR e.initiator.id IN :users) " +
+            "WHERE (:users IS NULL OR e.initiatorId IN :users) " +
             "AND (:states IS NULL OR e.state IN :states) " +
             "AND (:categories IS NULL OR e.category.id IN :categories) " +
             "AND e.eventDate BETWEEN :rangeStart AND :rangeEnd")
