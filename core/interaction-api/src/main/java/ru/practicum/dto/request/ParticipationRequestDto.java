fdaf221a158561b0f14e0a397a6c29d56964dac7
Denis585585 <Vegan585@yandex.ru>
2025-08-22 00:42:46 +0300

    Поменял ParticipationRequestDto

--- core/interaction-api/src/main/java/ru/practicum/dto/request/ParticipationRequestDto.java
+++ core/interaction-api/src/main/java/ru/practicum/dto/request/ParticipationRequestDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,8 +16,10 @@ public class ParticipationRequestDto {
     @NotNull(message = "Event ID cannot be null")
     private Long id;
     @NotNull
+    @JsonProperty("event")
     private Long eventId;
     @NotNull
+    @JsonProperty("requester")
     private Long requesterId;
     private RequestStatus status;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd' 'HH:mm:ss")
